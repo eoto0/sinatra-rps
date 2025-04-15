2ba3d86eeffdbbc67f890626b1eb74b14b664b21
eoto0 <eoto0@chicagobooth.edu>
2025-04-15 00:28:51 +0000

    final code

--- app.rb
+++ app.rb
@@ -7,20 +7,45 @@ end
 
 get("/rock") do
 
-# moves = ["rock", "paper", "scissors"]
-# @comp_move = moves.sample
+moves = ["rock", "paper", "scissors"]
+@comp_move = moves.sample
 
-# if @comp_move == "rock"
-#   @outcome = "tied"
-# elsif @comp_move == "paper"
-#   @outcome = "lost"
-# else 
-#   @outcome = "won"
-# end
-
-  erb(:zebra)
+if @comp_move == "rock"
+  @outcome = "tied"
+elsif @comp_move == "paper"
+  @outcome = "lost"
+else 
+  @outcome = "won"
 end
 
+  erb(:rock)
+end
+
+
 get("/paper") do
-  erb(:giraffe)
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "paper"
+    @outcome = "tied"
+  elsif @comp_move == "rock"
+    @outcome = "won"
+  else 
+    @outcome = "lost"
+  end
+ erb(:paper)
+end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "scissors"
+    @outcome = "tied"
+  elsif @comp_move == "rock"
+    @outcome = "lost"
+  else 
+    @outcome = "won"
+  end
+ erb(:scissors)
 end
